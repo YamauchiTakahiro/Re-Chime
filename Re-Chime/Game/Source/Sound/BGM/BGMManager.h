4ab3088101a4f638e6f99e444fe91604f8016e9d
kimurahayato-alpha <kimurahayato0102@gmail.com>
2026-03-19 11:57:04 +0900

    アウトゲームのBGM実装 (#14)
    Co-authored-by: KawaTaro <ca01254006@st.kawahara.ac.jp>

--- Re-Chime/Game/Source/Sound/BGM/BGMManager.h
+++ Re-Chime/Game/Source/Sound/BGM/BGMManager.h
@@ -1,5 +1,27 @@
 ﻿#pragma once
-class BGMManager
+#include "Source/Sound/BGM/BGMManager.h"
+
+//ファイルパスを追加する際は、
+//cppのnamespaceと下記のenumを追加してください。
+enum Sound {
+	enSound_TitleBGM,
+	enSound_GameOverBGM,
+	enSound_GameClearBGM,
+	enSound_Num //このステータスは、サウンドの総数を表しているため、この下には追加しないでください。
+};
+
+class BGMManager :public IGameObject
 {
+public:
+	BGMManager();
+	~BGMManager() {};
+	/// <summary>
+	/// 指定したサウンドを再生し、再生中のサウンドソースを返します。
+	/// </summary>
+	/// <param name="number">再生するサウンドを指定します。</param>
+	/// <param name="isLoop">サウンドをループ再生するかどうかを指定します。デフォルトは true です。</param>
+	/// <param name="volume">再生音量を指定します。デフォルトは 1.0f です。</param>
+	/// <returns>再生中のサウンドソースへのポインタ。</returns>
+	SoundSource* PlayingSound(Sound number, bool isLoop = true, float volume = 1.0f);
 };
 
--- Re-Chime/Game/Source/UIBase/Title/Title.h
+++ Re-Chime/Game/Source/UIBase/Title/Title.h
@@ -1,6 +1,9 @@
 ﻿#pragma once
 #include "Source/UIBase/UIBase.h"
 #include "Source/UIBase/Load/Load.h"
+#include "sound/SoundSource.h"
+
+
 class Title :
     public UIBase
 {
@@ -10,5 +13,7 @@ public:
     virtual bool Start() override;
     virtual void Update() override;
     virtual void Render(RenderContext& rc)override;
+private:
+    SoundSource* m_gameBGM;
 };
 
