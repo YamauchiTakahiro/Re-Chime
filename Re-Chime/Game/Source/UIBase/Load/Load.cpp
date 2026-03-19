b9d9f68084b06372820b7ea71b9439ed366bce92
takedatomohiro <ca01254012@st.kawahara.ac.jp>
2026-03-19 09:44:40 +0900

    ロード画面の実装 (#11)

--- Re-Chime/Game/Source/UIBase/Load/Load.cpp
+++ Re-Chime/Game/Source/UIBase/Load/Load.cpp
@@ -1,2 +1,37 @@
 ﻿#include "stdafx.h"
 #include "Load.h"
+#include "Game.h"
+
+Load::Load()
+{
+	m_Load.Init("Assets/Sprite/Load.DDs", 1920.0f, 1080.0f);
+	LoadTime = 10;
+}
+
+Load::~Load()
+{
+
+}
+
+bool Load::Start()
+{
+	return true;
+}
+
+void Load::Update()
+{
+	if (LoadTime > 0)
+	{
+		LoadTime--;
+	}
+	else
+	{
+		NewGO<Game>(0, "Game");
+		DeleteGO(FindGO<Load>("Load"));
+	}
+}
+
+void Load::Render(RenderContext& rc)
+{
+	m_Load.Draw(rc);
+}
--- Re-Chime/Game/Source/UIBase/Load/Load.h
+++ Re-Chime/Game/Source/UIBase/Load/Load.h
@@ -1,7 +1,15 @@
 ﻿#pragma once
 #include "Source/UIBase/UIBase.h"
-class Load :
-    public UIBase
+class Load :public UIBase
 {
+public:
+	Load();
+	~Load();
+	virtual bool Start() override;
+	virtual void Update() override;
+	virtual void Render(RenderContext& rc)override;
+
+private:
+	int LoadTime;
 };
 
