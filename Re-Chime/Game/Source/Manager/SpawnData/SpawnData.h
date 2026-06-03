0c930f1745ce2356acbc921775358bf27770a033
YamauchiTakahiro <ca01254022@st.kawahara.ac.jp>
2026-06-03 10:25:44 +0900

    ロード時間の短縮、軽量化 (#109)

---
+++ Re-Chime/Game/Source/Manager/SpawnData/SpawnData.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "stdafx.h"
+
+enum class enSpawnType
+{
+	enSpawnType_SmallRobot,
+	enSpawnType_MediumRobot,
+	enSpawnType_FloorBoss,
+	enSpawnType_FinalBoss,
+	enSpawnType_Barrier,
+};
+
+struct SpawnData
+{
+	enSpawnType spawnType;
+	Vector3 pos;
+	Quaternion rot;
+	Vector3 scale;
+	int floorNum;
+	bool spawned = false;
+};
